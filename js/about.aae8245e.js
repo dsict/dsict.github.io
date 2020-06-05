cc89f770ef6613cf4afc1c649b2bacabb409af83
Jenny <kja0717@gmail.com>
2020-06-05 11:53:02 +0900

    Add files via upload

---
+++ js/about.aae8245e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" This is About Page ")])},s=[],a=t("2877"),i={},l=Object(a["a"])(i,u,s,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=about.aae8245e.js.map
